de35f060f0e0b5cac29f8fef84505c733b050fa1
Sanjay Khatri <sanzaikc@gmail.com>
2020-03-27 08:19:51 +0545

    display all question

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container flex">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">Questions</div>
+
+                <div class="card-body">
+                    @foreach ($questions as $question )
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0"> {{ $question->title }} </h3>
+                               {{  Str::limit($question->body , 250) }}
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
