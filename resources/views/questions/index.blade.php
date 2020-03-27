732cb26ddb01e8c69a4c3cd8fecee14df5294498
Sanjay Khatri <sanzaikc@gmail.com>
2020-03-27 09:30:09 +0545

    Dynamic votes and answer counter in question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,13 +3,24 @@
 @section('content')
 <div class="container flex">
     <div class="row justify-content-center">
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Questions</div>
 
                 <div class="card-body">
                     @foreach ($questions as $question )
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong> {{ $question->votes }} </strong> {{ Str::plural('vote', $question->votes) }}
+                                </div>
+                            <div class="status {{ $question->status }}">
+                                    <strong> {{ $question->answers }} </strong> {{ Str::plural('answer', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                     {{ $question->views ." ". Str::plural('view', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                             <h3 class="mt-0"> <a href=" {{ $question->url}}">{{ $question->title }}</a> </h3>
                             <p class="lead">
